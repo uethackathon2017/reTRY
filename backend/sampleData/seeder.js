fb71e1578dd863423c4500eb8457a5447395acf1
Dang Hai-Trieu <dangtrieu25@gmail.com>
2017-03-11 00:09:11 +0700

    add: seed users

--- backend/sampleData/seeder.js
+++ backend/sampleData/seeder.js
@@ -4,7 +4,7 @@ const Promise = require('bluebird');
 const jsonFile = Promise.promisifyAll(require('jsonfile'));
 const fs = Promise.promisifyAll(require('fs'));
 const faker = require('faker');
-const { Topic, Word, Quiz, mongoInit } = require('../models');
+const { User, Topic, Word, Quiz, mongoInit } = require('../models');
 
 const topicPath = __dirname + '/raw/topic/topics.json';
 const vocabPath = __dirname + '/raw/vocabulary';
@@ -12,9 +12,14 @@ const vocabPath = __dirname + '/raw/vocabulary';
 mongoInit(err => {
   if (err)
     throw err;
-  console.log('Cleaning "Topic" table...');
-  Topic
+  console.log('Cleaning "User" table...');
+  User
     .removeAsync()
+    .then(() => {
+      console.log('Documents on "User" table cleared!');
+      console.log('Cleaning "Topic" table...');
+      return Topic.removeAsync();
+    })
     .then(() => {
       console.log('Documents on "Topic" table cleared!');
       console.log('Cleaning "Word" table...');
@@ -27,6 +32,31 @@ mongoInit(err => {
     })
     .then(() => {
       console.log('Documents on "Quiz" table cleared!');
+      console.log('Importing users...');
+      let userBulk = [];
+      for (let idx = 0; idx < 50; idx++) {
+        let newUser = {
+          fbId: '2139019283102' + idx,
+          email: faker.internet.email(),
+          fbLink: faker.internet.url(),
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          pictureURL: faker.image.imageUrl(),
+          gender: faker.random.boolean() ? 'male' : 'female',
+          friendList: [],
+          level: faker.random.number({min: 1, max: 50}),
+          score: faker.random.number({min: 50, max: 300}),
+          awards: [],
+          words: [],
+          role: 'common',
+          isActive: true
+        };
+        userBulk.push(newUser);
+      }
+      return User.insertManyAsync(userBulk);
+    })
+    .then(users => {
+      console.log(users.length + ' user(s) inserted!');
       return jsonFile.readFileAsync(topicPath);
     })
     .then(topics => {
@@ -34,7 +64,7 @@ mongoInit(err => {
       return Promise.join(Topic.insertManyAsync(topics), fs.readdirAsync(vocabPath));
     })
     .then(([ topics, topicFolders ]) => {
-      console.log(topics.length + ' topics inserted!');
+      console.log(topics.length + ' topic(s) inserted!');
       console.log('Importing words...');
       let wordObjects = {};
       let words = [];
@@ -59,7 +89,7 @@ mongoInit(err => {
       return Word.insertManyAsync(words);
     })
     .then(result => {
-      console.log(result.length + ' words inserted!');
+      console.log(result.length + ' word(s) inserted!');
       console.log('Importing quizes...');
       let quizBulk = [];
       for (let idx = 0; idx < result.length - 4; idx++) {
