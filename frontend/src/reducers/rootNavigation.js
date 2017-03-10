b2e99c3cb2aa3f9400c0631c83966d2a76d3ea96
reTRY <retry@darkmoon.us>
2017-03-10 19:46:11 +0700

    natigation init

---
+++ frontend/src/reducers/rootNavigation.js
@@ -0,0 +1,24 @@
+import {cardStackReducer} from 'react-native-navigation-redux-helpers';
+
+const initialState = {
+    key: 'root',
+    index: 0,
+    routes: [
+        {
+            key: 'login',
+            index: 0,
+        },
+    ],
+};
+
+export default cardStackReducer(initialState);
+
+
+//Selectors:
+export const getRootNavigation = (state) => {
+    return state;
+};
+
+export const getCurrentRootNavigationRouteKey = (state) => {
+    return state.routes[state.routes.length - 1].key;
+};
