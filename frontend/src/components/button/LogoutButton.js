e39a230826b240cdb7363e40d306656414a227d5
nvn01234 <nguyenvannhat152@gmail.com>
2017-03-11 21:14:23 +0700

    sign out button

---
+++ frontend/src/components/button/LogoutButton.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {Button, Icon} from 'native-base';
+import {connect} from 'react-redux';
+import {logout} from '../../actions/login'
+import {navPopRoute, navReplaceAt} from '../../actions/rootNavigation'
+
+class BackButton extends Component {
+    _logout() {
+        this.props.logout();
+        this.props.navPopRoute();
+    }
+
+    render() {
+        return (
+            <Button light transparent onPress={() => this._logout()}>
+                {/*<Icon name='exit-to-app' android="md-exit" ios="ios-exit"*/}
+                {/*style={{color: 'white', alignSelf: 'center'}}/>*/}
+                <Text style={{color: 'white', alignSelf: 'center'}}>Sign out</Text>
+            </Button>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {}
+};
+
+export default connect(mapStateToProps, {
+    logout, navPopRoute
+})(BackButton);
