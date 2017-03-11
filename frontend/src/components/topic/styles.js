cbd6845d85ba947ae80d4c7ff7cf3d3fd6f0615d
nvn01234 <nguyenvannhat152@gmail.com>
2017-03-12 02:31:58 +0700

    làm màu

--- frontend/src/components/topic/styles.js
+++ frontend/src/components/topic/styles.js
@@ -13,110 +13,54 @@ export default StyleSheet.create({
         justifyContent: 'center',
     },
 
-    titleRow: {
-        width: screenWidth,
+    topic: {
+        flex: 1,
+        width: screenWidth - 20,
+        flexDirection: 'row',
+        padding: 20,
+        minHeight: 100,
+        backgroundColor: 'white',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+
+    topicTouchable: {
+        marginTop: 10,
+        marginBottom: 10,
+    },
+
+    topicIconContainer: {
+        flex: 0.2,
         alignItems: 'center',
         justifyContent: 'center',
-        paddingTop: 10,
-        paddingBottom: 10,
     },
 
-    title: {
-        ...fromTheme.BG_TRANSPARENT,
-        color: 'white',
-        fontSize: 20,
-        fontWeight: 'bold',
-        alignSelf: 'center',
-    },
-
-    statusBarBackground: {
-        width: screenWidth,
-        height: fromTheme.STATUSBAR_HEIGHT,
-        backgroundColor: fromTheme.ULTRAMARINE,
-    },
-
-    userAvatar: {
-        marginTop: 20,
-        marginBottom: 20,
-        width: 100,
-        height: 100,
-        borderRadius: 50,
-        borderWidth: 2,
+    topicIcon: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 1,
         borderColor: 'white',
         alignSelf: 'center',
     },
 
-    userAvatarContainer: {},
-
-    userName: {
-        ...fromTheme.BG_TRANSPARENT,
-        alignSelf: 'center',
-        color: 'white',
-        fontSize: 16,
-    },
-
-    userLevelContainer: {
-        marginTop: 20,
-        marginBottom: 20,
-        width: screenWidth,
-        paddingLeft: 10,
-        paddingRight: 10,
-    },
-
-    userLevelProgressContainer: {
-        flexDirection: 'row',
-    },
-
-    userLevelProgress: {
+    topicBody: {
         flex: 0.8,
-        alignItems: 'center',
-        justifyContent: 'center',
+        flexDirection: 'column',
+        paddingLeft: 10,
     },
 
-    userScore: {
+    topicTitleContainer: {
+        flex: 0.4,
+        alignItems: 'flex-start',
+    },
+
+    topicDescriptionContainer: {
+        flex: 0.6,
+    },
+
+    topicTitle: {
         ...fromTheme.BG_TRANSPARENT,
-        color: 'white',
-        alignSelf: 'center',
-    },
-
-    userLevel: {
-        ...fromTheme.BG_TRANSPARENT,
-        flex: 0.1,
-        color: 'white',
-        textAlign: 'center',
-    },
-
-    achievementList: {
-        width: screenWidth,
-    },
-
-    achievementListTitle: {
-        ...fromTheme.BG_TRANSPARENT,
-        color: 'white',
-        fontWeight: 'bold',
-        fontSize: 20,
-    },
-
-    achievementListTitleContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: screenWidth,
-        backgroundColor: fromTheme.BLACK_CHERRY,
-        paddingTop: 5,
-        paddingBottom: 5,
-        marginBottom: 10,
-    },
-
-    achievementCard: {
-        width: screenWidth - 32,
-        marginLeft: 16,
-        marginRight: 16,
-        backgroundColor: 'white',
-    },
-
-    achievementTitle: {
-        ...fromTheme.BG_TRANSPARENT,
-        color: fromTheme.BLACK_CHERRY,
-        fontWeight: 'bold',
+        color: 'black',
     },
 });
